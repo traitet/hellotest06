3048edcea357888c58941965ab704b615f13b086
Traitet <traitet@gmail.com>
2020-05-17 02:01:35 +0700

    edit order

---
+++ lib/screens_seerest/ROrderItemEditPage.dart
@@ -0,0 +1,149 @@
+  //========================================================
+  // IMPORT
+  //========================================================  
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:hellotest06/models_seerest/ROrderModel.dart';
+import '../models_seerest/RMenuModel.dart';
+import '../services/LoggerService.dart';
+import '../services/ShowNotification.dart';
+
+  //======================================================================================
+  // MAIN CLASS
+  //======================================================================================  
+class ROrderItemEditPage extends StatefulWidget {
+  //======================================================================================
+  // VARIABLE AND CONSTUCTURE
+  //======================================================================================    
+  final String menuId;
+  final String orderItemId;
+  ROrderItemEditPage({Key key, @required this.menuId, @required this.orderItemId}): super(key: key);  
+  //======================================================================================
+  // OVERRICE BUILD
+  //======================================================================================    
+  @override
+  _ROrderItemEditPageState createState() => _ROrderItemEditPageState();
+}
+//==========================================================
+// STATE CLASS
+//========================================================================================  
+class _ROrderItemEditPageState extends State<ROrderItemEditPage> {
+  String _uploadedFileURL = '';  
+  var snapshotMenuCatList;
+  ROrderModel  _model; 
+ 
+  //======================================================================================
+  // DECALRE VARIABLE
+  //======================================================================================  
+  final _idController = TextEditingController()..text = '';
+  final _nameController = TextEditingController()..text = 'Fried rice';
+  final _descriptionController = TextEditingController()..text = 'ข้าวผัด';
+  final _imageUrlController = TextEditingController()..text = 'www.imagefoodmenu.com';   
+  final _priceController = TextEditingController()..text = '100';
+  final _qtyController = TextEditingController()..text = '4';  
+  final _menuIdController = TextEditingController()..text = 'MENU ID';  
+
+//=========================================================================================
+// 4) GET DATA FROM DB ?? YES
+//=========================================================================================
+  @override
+  void initState() {
+    super.initState();
+    logger.i(widget.menuId);
+    Firestore.instance.collection("TT_ORDER").document(widget.orderItemId).get().then((value) {
+      setState(() {
+        //=================================================================================
+        // 4) GET DATA FROM DB ?? YES
+        //=================================================================================       
+        _model = ROrderModel.fromFilestore(value);  
+        _idController.text = _model.id;
+        _menuIdController.text = _model.menuId;
+        _nameController.text = _model.name;
+        _descriptionController.text = _model.description;
+        _imageUrlController.text = _model.imageUrl;
+        _uploadedFileURL = _model.imageUrl;
+        _qtyController.text = _model.qty.toString();        
+        logger.i(value.data.toString());        
+      });
+    });
+  }
+
+  //=========================================================================================
+  // OVERRIDE (IMPLEMENT UI)
+  //=========================================================================================   
+  @override
+  Widget build(BuildContext context) {         
+    return Scaffold(
+      appBar: AppBar(title: Text('Edit Order: ' + widget.menuId),),    
+      body: ListView(
+        children: <Widget>[
+          _idController.text == '' ? Text("Loading . . . "):
+          //================================================================================
+          // BUILD WIDGET IMAGE AND TEXT (1) UPLOAD 2) GOOGLE 3) WRONG URL
+          //================================================================================
+            _uploadedFileURL != '' ? Container(padding: const EdgeInsets.all(8.0),height: 200,child: Image.network(_imageUrlController.text)):
+            widgetBodyImage(),     
+    
+          //================================================================================= 
+          // UI: TEXT
+          //=================================================================================  
+          TextFormField(decoration: InputDecoration(labelText: 'Menu ID', prefixIcon: Icon(Icons.insert_chart)),controller: _menuIdController,),
+          TextFormField(decoration: InputDecoration(labelText: 'Menu Name', prefixIcon: Icon(Icons.insert_chart)),controller: _nameController,),
+          TextFormField(decoration: InputDecoration(labelText: 'Menu Description', prefixIcon: Icon(Icons.insert_chart)),controller: _descriptionController,),
+          TextFormField(decoration: InputDecoration(labelText: 'Price', prefixIcon: Icon(Icons.insert_chart)),controller:_priceController,), 
+          TextFormField(decoration: InputDecoration(labelText: 'Qty', prefixIcon: Icon(Icons.insert_chart)),controller:_qtyController,keyboardType: TextInputType.number,),           
+          //=================================================================================
+          // UI: SAVE BUTTON
+          //=================================================================================                      
+          RaisedButton(onPressed: (){fnSave();}, child: Text('SAVE'),),               
+        ],
+      ),
+    );
+  }
+ 
+  //============================================================================================
+  // SAVE (COLLECTION=TABLE, DOCUMENT=PK)
+  //============================================================================================   
+  fnSave() async {
+    //==========================================================================================
+    // PREPARE DATA
+    //==========================================================================================       
+    ROrderModel myModel = ROrderModel(
+      id: _idController.text,
+      name: _nameController.text, 
+      description: _descriptionController.text, 
+      imageUrl: _uploadedFileURL,    
+    );
+    logger.i(myModel);
+    //===========================================================================================
+    // SHOW LOG
+    //===========================================================================================   
+    logger.i(myModel.toFileStone());
+    //===========================================================================================
+    // SAVE DB TO FIRESTORE
+    //===========================================================================================     
+    Firestore.instance.collection("TT_ORDER").document(_idController.text).setData(myModel.toFileStone())        // SAVE DB
+    .then((returnDocuments){        // IF COMPLETE
+      logger.i('Insert Complete');  // PRINT LOG
+      showMessageBox(context, "Success", _idController.text + " saved completely", actions: [dismissButton(context)]);   //POP UP COMPLETE
+    }
+    ).catchError((e){               // IF ERROR
+        logger.e("Insert Error");   // PRINT LOG
+    }); 
+  }
+
+  //============================================================================================
+  // WIDGET:IMAGE BODY WIDGET
+  //============================================================================================
+  Widget widgetBodyImage() => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset('assets/images/bg01.jpg',
+            width: 300, height: 200, fit: BoxFit.cover),
+  );
+
+
+
+
+
+}
--- lib/screens_seerest/ROrderPage..dart
+++ lib/screens_seerest/ROrderPage..dart
@@ -4,6 +4,10 @@
 //========================================================  
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import '../screens_seerest/ROrderItemEditPage.dart';
+import '../services/LoggerService.dart';
+import '../services/ShowNotification.dart';
+import '../widgets_seedoc/BadgeIcon.dart';
 // import '../models_seerest/ROrderModel.dart';
 
 
@@ -15,28 +19,35 @@ class ROrderPage extends StatefulWidget {
   _ROrderPageState createState() => _ROrderPageState();
 }
 
-//========================================================
+//=============================================================================================
 // STATE CLASS
-//========================================================  
+//=============================================================================================  
 class _ROrderPageState extends State<ROrderPage> {
   int index = 0;
 
-  //========================================================
+  //===========================================================================================
   // OVERRICE
-  //========================================================  
+  //===========================================================================================  
   @override
   void initState() {
     super.initState();
   }
 
-  //========================================================
+  //===========================================================================================
   // BUILD UI
-  //======================================================== 
+  //=========================================================================================== 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Order Items'),
+        actions: <Widget>[IconButton(
+            icon: StreamBuilder(
+            stream:  Firestore.instance.collection("TT_ORDER").snapshots(),
+            builder: (BuildContext context, AsyncSnapshot _shapshot) => BadgeIcon(icon:Icon(Icons.add_shopping_cart, size: 25,),
+            badgeCount:  _shapshot.data.documents.length??0        
+        ), )
+        , onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => ROrderPage()),);}     ),],
       ),
       body: StreamBuilder(
          stream: Firestore.instance.collection("TT_ORDER").snapshots(),
@@ -59,7 +70,8 @@ class _ROrderPageState extends State<ROrderPage> {
                     String _description =  snapshot.data.documents[index].data["description"];  
                     int _qty =  snapshot.data.documents[index].data["qty"]??1;     
                     String _table =  snapshot.data.documents[index].data["table"]??'';  
-                    String _imageUrl =  snapshot.data.documents[index].data["_imageUrl"]??'';                                                                                                       
+                    String _imageUrl =  snapshot.data.documents[index].data["_imageUrl"]??'';    
+                    String _menuId =  snapshot.data.documents[index].data["_menuId"]??'';                                                                                                                          
                 return Padding(padding: const EdgeInsets.all(2.0),
                   child: Card(  
                     child: Container(                   
@@ -68,7 +80,7 @@ class _ROrderPageState extends State<ROrderPage> {
                         child: Column(
                           children: <Widget>[  
  
-                            widgetBodyText(_docId,_name,_description,_qty,_table,_imageUrl),        
+                            widgetBodyText(_docId,_name,_description,_qty,_table,_imageUrl, _menuId),        
                           ],
                         ),
                       ),
@@ -85,7 +97,7 @@ class _ROrderPageState extends State<ROrderPage> {
   //======================================================
   // WIDGET: BODY TEXT
   //======================================================
-  Widget widgetBodyText(String myDocId,String myName, String myDescription, int myQty, String myTable,String myImageUrl)  { 
+  Widget widgetBodyText(String myDocId,String myName, String myDescription, int myQty, String myTable,String myImageUrl, String myMenuId)  { 
   return 
     Container(
       child: Padding(padding: const EdgeInsets.all(4),
@@ -100,9 +112,17 @@ class _ROrderPageState extends State<ROrderPage> {
                   ), 
                 ),
                 Image.network(myImageUrl),
-                Column(children: <Widget>[
-                  
-                  IconButton(onPressed: (){},icon: Icon(Icons.edit),color: Colors.orange,),
+                Row(children: <Widget>[
+                  IconButton(onPressed: (){
+                      Firestore.instance.collection("TT_ORDER").document(myDocId).delete();
+                      showMessageBox(context, "success","Cancel completely",actions: [dismissButton(context)]);                      
+                      logger.i(myDocId + 'deleted completly');
+                  },icon: Icon(Icons.remove_circle),color: Colors.orange,),
+                  IconButton(onPressed: (){
+
+                  Navigator.push(context,MaterialPageRoute(builder: (context) => ROrderItemEditPage(menuId: myDocId,orderItemId: myDocId)));       
+
+                  },icon: Icon(Icons.edit),color: Colors.orange,),
                 ],)
 
           ],
